e7468bf4ec44c56b464d1b24a08652027f07c30e
Brian Ondyak <brianondyak@users.noreply.github.com>
2016-11-08 17:44:42 -0600

    Commit

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,29 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @num = params[:id].to_f.round(2)
+    @result = Math.sqrt(@num).round(2)
+  end
+
+  def random
+    @low = params[:lo].to_i
+    @low_adj = @low + 1
+    @high = params[:hi].to_i
+    @result = rand(@low_adj...@high)
+  end
+
+  def payment
+    @apr = params[:rate].to_f
+    @apr = @apr / 100
+    @year = params[:year].to_i
+    @principal = params[:principal].to_f.round(0)
+
+    @rate = @apr/100/12
+
+    @result = (@rate*@principal)/(1 - (1+@rate)**(-@year*12))
+    @result = @result.round(2)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+
+  get("/random/:lo/:hi", { :controller => "calculations", :action => "random" })
+
+  get("/payment/:rate/:year/:principal", { :controller => "calculations", :action => "payment" })
 end
